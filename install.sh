127b501cf07cb9552f1c71d653281b7d81449cc9
Lasse S. Haslev <lasse@haslev.no>
2016-11-23 17:29:01 +0100

    Added backslashes

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 # Get the information for this package
 echo "What is the namespace of this package (LasseHaslev\\\\ExampleName)"
-read namespace;
+read -r namespace;
 
 # Change the namespace
 sed -i '' -e "s/\%namespace\%/$namespace/g" './src/Providers/ServiceProvider.php'
@@ -14,7 +14,7 @@ sed -i '' -e "s/\%namespace\%/$namespace/g" './composer.json'
 echo "I am sorry, but you have to add another backward slash in ./composer.json on line 17."
 
 # echo "What is the name of the config file ( packagename )"
-read packagename;
+read -r packagename;
 
 # rename the config file
 mv ./config/my_package.php ./config/$packagename.php # works
