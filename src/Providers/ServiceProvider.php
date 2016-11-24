0f852ae78f18fdd480b4c466a41cc0a9071e8ff0
Lasse S. Haslev <lasse@haslev.no>
2016-11-24 08:15:39 +0100

    Fixed small hickup of fieldable name still being used

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -15,7 +15,7 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom( __DIR__.'/../../config/fieldable.php', '%packagename%');
+        $this->mergeConfigFrom( __DIR__.'/../../config/%packagename%.php', '%packagename%');
     }
 
     /**
